39548afdc148ede1de8e869269ebff1e0b70906a
Omer <barakom@post.bgu.ac.il>
2020-01-06 07:06:55 +0200

    code decoration for ConnectionsImpl and Stomp encDec

--- src/main/java/bgu/spl/net/impl/stomp/MessageEncoderDecoderStomp.java
+++ src/main/java/bgu/spl/net/impl/stomp/MessageEncoderDecoderStomp.java
@@ -9,6 +9,7 @@ public class MessageEncoderDecoderStomp implements MessageEncoderDecoder<String>
     private byte[] bytes = new byte[1 << 10]; //start with 1k
     private int len = 0;
     private boolean firstPartTofinish=false;
+
     @Override
     public String decodeNextByte(byte nextByte) {
         if(nextByte!='@'&&firstPartTofinish){
@@ -16,9 +17,7 @@ public class MessageEncoderDecoderStomp implements MessageEncoderDecoder<String>
         }
         if (nextByte == '^') {
             firstPartTofinish=true;
-
         }
-
         if(nextByte == '@'&&firstPartTofinish){
             pushByte(nextByte);
             return popString();
@@ -38,12 +37,10 @@ public class MessageEncoderDecoderStomp implements MessageEncoderDecoder<String>
         if (len >= bytes.length) {
             bytes = Arrays.copyOf(bytes, len * 2);
         }
-
         bytes[len++] = nextByte;
     }
 
     private String popString() {
-
         String result = new String(bytes, 0, len, StandardCharsets.UTF_8);
         len = 0;
         return result;
--- src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
+++ src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -7,25 +7,23 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionsImpl<T> implements Connections<T> {
-    ConcurrentHashMap<Integer, NonBlockingConnectionHandler<T>>  clientHashMap;
-    ConcurrentHashMap<String, ReadOrWriteHandlerList<T>> channelsMap;
+    private ConcurrentHashMap<Integer, NonBlockingConnectionHandler<T>>  clientHashMap; //map for the connection handler for each connected client
+    private ConcurrentHashMap<String, ReadOrWriteHandlerList<T>> channelsMap;   //map for the clients of each channel
 
 
     @Override
-    public boolean send(int connectionId, T msg) {
+    public boolean send(int connectionId, T msg) {  //send a message to a single client
         NonBlockingConnectionHandler<T> handler= clientHashMap.get(connectionId);
         if(handler==null) {
             System.out.println("Error: Tried to send a message to a non existent user");
             return false;
         }
-        else{
-            handler.send(msg);
-            return true;
-        }
+        handler.send(msg);
+        return true;
     }
 
     @Override
-    public void send(String channel, T msg) {
+    public void send(String channel, T msg) {   //send a message to all clients of a certain channel
         ReadOrWriteHandlerList<T> channelList=channelsMap.get(channel);
         for(int i=0;i<channelList.size();++i){
             channelList.sendAtIndexi(i,msg);
@@ -38,6 +36,5 @@ public class ConnectionsImpl<T> implements Connections<T> {
         for (ReadOrWriteHandlerList<T> tReadOrWriteHandlerList : t) {
             tReadOrWriteHandlerList.disconnectIDi(connectionId);
         }
-
     }
 }
